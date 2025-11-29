2ea3c8dacda06611dd02a585c8a95d2d8c080b33
Cyb3r-Bishop <ariffoysalruetcse21@gmail.com>
2025-11-30 02:20:49 +0600

    Fix: Correct base-href to match repository name@ (Christian-Daidone-demo)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "152b52f724bef60563c08d9eb7b80737
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "72259f2c1ebb760b3a28a8c0d00f17ae",
+"flutter_bootstrap.js": "450a904a193129718423ffcb1d356352",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
